e46f2bafe5761157bc194508251d2996067f0602
Li-Zhenghao-Harbin <dream7c@126.com>
2025-12-01 15:58:01 +0800

    Format tax refund mapper

--- src/main/java/org/cityu/dao/TaxRefundMapper.java
+++ src/main/java/org/cityu/dao/TaxRefundMapper.java
@@ -4,13 +4,5 @@ import java.util.List;
 import org.cityu.dataobject.TaxRefundDO;
 
 public interface TaxRefundMapper {
-    int deleteByPrimaryKey(Integer id);
-
     int insert(TaxRefundDO record);
-
-    TaxRefundDO selectByPrimaryKey(Integer id);
-
-    List<TaxRefundDO> selectAll();
-
-    int updateByPrimaryKey(TaxRefundDO record);
 }
